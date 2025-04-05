447c8347b2de34b3b7a5fd92393d5de3213dafbe
Abhay Jha <54393533+AJ-Walker@users.noreply.github.com>
2025-04-05 20:09:18 +0530

    fixed some bug on main.tf (#1)
    Co-authored-by: root <root@LAPTOP-BVMEV4GH>

--- aws-infra/main.tf
+++ aws-infra/main.tf
@@ -41,37 +41,6 @@ resource "aws_s3_bucket_policy" "allow_get_images_policy" {
   policy = data.aws_iam_policy_document.allow_get_s3_images_policy.json
 }
 
-
-  tags = {
-    Name        = "Movies REST API"
-    Environment = "Dev"
-  }
-}
-
-resource "aws_s3_object" "movies_rest_api_images" {
-  for_each     = local.files
-  bucket       = aws_s3_bucket.movies_rest_api_bucket.id
-  key          = "${var.s3_images_prefix}/${each.value}"
-  source       = "${path.module}/${var.local_images_folder}/${each.value}"
-  etag         = filemd5("${path.module}/${var.local_images_folder}/${each.value}")
-  content_type = "application/octet-stream"
-}
-
-resource "aws_s3_bucket_public_access_block" "movies_rest_api_bucket_public_access" {
-  bucket = aws_s3_bucket.movies_rest_api_bucket.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-
-}
-
-resource "aws_s3_bucket_policy" "allow_get_images_policy" {
-  bucket = aws_s3_bucket.movies_rest_api_bucket.id
-  policy = data.aws_iam_policy_document.allow_get_s3_images_policy.json
-}
-
 # DynamoDB
 resource "aws_dynamodb_table" "movies_db" {
   name         = "Movies"
